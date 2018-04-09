10ebdc6f5492c166053b500ba0aae28d5e4ff691
gefcho <www_gef_12@mail.ru>
2018-04-09 21:43:17 +0300

    create project

---
+++ common/js/em.nicescroll.js
@@ -0,0 +1,34 @@
+//jquery.nicescroll 3.5.4
+(function($) {
+
+$(document).ready(function(){
+	$("html").niceScroll({
+		background: 'none repeat scroll 0 0 rgba(0, 0, 0, 0.2)',
+		scrollspeed: 50,
+		mousescrollstep: 50,
+		cursorwidth: 6,
+		cursorborder: 0,
+		cursorcolor: '#3399ff',
+		autohidemode: false,
+		zindex: 9999999,
+		horizrailenabled: false,
+		cursorborderradius: 0
+	});
+});
+
+$(document).ready(function(){
+	$(".nicescroll").niceScroll({
+		background: 'none repeat scroll 0 0 rgba(0, 0, 0, 0.2)',
+		scrollspeed: 50,
+		mousescrollstep: 50,
+		cursorwidth: 4,
+		cursorborder: 0,
+		cursorcolor: '#3399ff',
+		autohidemode: false,
+		zindex: 9999999,
+		horizrailenabled: false,
+		cursorborderradius: 0
+	});
+});
+
+})(jQuery);
